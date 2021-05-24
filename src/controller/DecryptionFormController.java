3c4651cd253922d486057d62a3f2fb995169bf03
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-24 18:46:07 +0530

    Project Structure

---
+++ src/controller/DecryptionFormController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class DecryptionFormController {
+    public TextField txtText;
+    public TextField txtKey;
+    public TextField txtCipher;
+
+    public void decriptOnAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/controller/EncryptionFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class EncryptionFormController {
+
+    public TextField txtText;
+    public TextField txtKey;
+    public TextField txtCipherText;
+
+    public void encriptOnAction(ActionEvent actionEvent) {
+
+    }
+
+}
