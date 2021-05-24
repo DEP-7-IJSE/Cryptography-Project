463a9c222d9ab0a3e655c3fb47d2a684d3177377
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-24 18:23:55 +0530

    MainForm Done

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class MainFormController {
+    public void encriptOnAction(ActionEvent actionEvent) {
+    }
+
+    public void decriptOnAction(ActionEvent actionEvent) {
+
+    }
 }
