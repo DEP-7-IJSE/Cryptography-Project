a6bda7ebe985e2f6e7dda015ac98a5f4b18b6544
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-24 19:01:37 +0530

    MainForm completed

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,12 +1,35 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
-    public void encriptOnAction(ActionEvent actionEvent) {
+    public AnchorPane root;
+
+    public void encriptOnAction(ActionEvent actionEvent) throws IOException {
+        loadForm("/view/EncryptionForm.fxml","Encription Window");
     }
 
-    public void decriptOnAction(ActionEvent actionEvent) {
+    public void decriptOnAction(ActionEvent actionEvent) throws IOException {
+        loadForm("/view/DecryptionForm.fxml","Decription Window");
+    }
 
+    private void loadForm(String loadFormURL,String windowName) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(this.getClass().getResource(loadFormURL));
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle(windowName);
+        stage.centerOnScreen();
+        stage.initOwner(this.root.getScene().getWindow());
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.show();
     }
 }
