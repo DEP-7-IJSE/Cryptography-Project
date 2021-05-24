7598744e3f828275eede9fbe80ec2425e7ef54eb
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-05-24 23:50:06 +0530

    Created jar file

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
-import java.util.Random;
+import static lk.ijse.crypto.CryptoAlgo.decrypt;
 
 public class DecryptionFormController {
     public TextField txtText;
@@ -26,40 +26,7 @@ public class DecryptionFormController {
             txtKey.requestFocus();
             return;
         }
-        if(cipher.length()<15){
-            genRandomNumbers();
-            return;
-        }
-        String dec = cipher.substring(3,cipher.length()-12);
-        String reverseText="";
-        for (int i = 0; i < dec.length(); i++) {
-            int code=dec.charAt(i);
-            code-=10;
-            char originalChar = (char) code;
-            reverseText+=originalChar;
-        }
-        String gotreversedText=reverseText.substring(2,reverseText.length()-3);
-        String[] splitted=gotreversedText.split("@",3);
-        String reversedKey="";
-        for (int i = splitted[0].length()-1; i >=0; i--) {
-            reversedKey+=splitted[0].charAt(i);
-        }
-        if(reversedKey.equals(txtKey.getText())){
-            String text="";
-            for (int i = splitted[1].length()-1; i >=0; i--) {
-                text+=splitted[1].charAt(i);
-            }
-            txtText.setText(text);
-        }else{
-            genRandomNumbers();
-            return;
-        }
-    }
-    private void genRandomNumbers(){
-        Random random= new Random();
-        int randomInt = random.nextInt();
-        char randomChar = (char) randomInt;
-        txtText.setText(String.valueOf(randomChar));
+        txtText.setText(decrypt(cipher,key));
     }
 
     public void keyEnteredOnAction(ActionEvent actionEvent) {
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
+import static lk.ijse.crypto.CryptoAlgo.encrypt;
+
 public class EncryptionFormController {
 
     public TextField txtText;
@@ -25,21 +27,7 @@ public class EncryptionFormController {
             txtKey.requestFocus();
             return;
         }
-        text+="@"+key;
-        String reversedText="@@";
-        for (int i = text.length()-1; i >=0; i--) {
-            reversedText+=text.charAt(i);
-        }
-        reversedText+="@!!";
-        String cipheText="";
-        for (int i = 0; i < reversedText.length(); i++) {
-            int code=reversedText.charAt(i);
-            code+=10;
-            char newChar = (char) code;
-            cipheText+=newChar;
-        }
-        cipheText+=cipheText.hashCode();
-        txtCipherText.setText("^&5"+cipheText+"^%");
+        txtCipherText.setText(encrypt(text,key));
     }
 
     public void keyEnteredOnAction(ActionEvent actionEvent) {
